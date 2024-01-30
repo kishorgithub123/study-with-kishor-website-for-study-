3adf60bbe5e7e287c47aed28f244de4f0599be1e
kishorgithub123 <142067255+kishorgithub123@users.noreply.github.com>
2024-01-30 10:51:44 +0530

    Add files via upload

---
+++ study.js
@@ -0,0 +1,71 @@
+let math=document.querySelector("#math")
+math.onclick=()=>{
+    alert("are you sure you want to learn from this website")
+    document.write("welcome to your new bright future")
+}
+let res=document.querySelector("#res")
+res.onclick=()=>{
+    alert("are you sure you want to learn")
+    
+}
+let his=document.querySelector("#his")
+his.onclick=()=>{
+    alert("are you sure you want to learn")
+    
+}
+let eco=document.querySelector("#eco")
+eco.onclick=()=>{
+    alert("are you sure you want to learn")
+    
+}
+let pol=document.querySelector("#pol")
+pol.onclick=()=>{
+    alert("are you sure you want to learn")
+    
+}
+let geo=document.querySelector("#geo")
+geo.onclick=()=>{
+    alert("are you sure you want to learn")
+    
+}
+let aff=document.querySelector("#aff")
+aff.onclick=()=>{
+    alert("are you sure you want to learn")
+    
+}
+let ht=document.querySelector("#ht")
+ht.onclick=()=>{
+    alert("go to learn html")
+    // document.write("this site is not working yet!!!!")
+}
+let css=document.querySelector("#css")
+css.onclick=()=>{
+    alert("go to learn css")
+    // document.write("this site is not working yet!!!!")
+}
+let js=document.querySelector("#js")
+js.onclick=()=>{
+    alert("go to learn js")
+    // document.write("this site is not working yet!!!!")
+}
+let java=document.querySelector("#java")
+java.onclick=()=>{
+    alert("go to learn java with dsa")
+    // document.write("this site is not working yet!!!!")
+}
+let my=document.querySelector("#my")
+my.onclick=()=>{
+    alert("go to learn mysql")
+    // document.write("this site is not working yet!!!!")
+}
+let cpp=document.querySelector("#cpp")
+cpp.onclick=()=>{
+    alert("go to learn cpp");
+    // document.write("this site is not working yet!!!!")
+}
+let py=document.querySelector("#py")
+py.onclick=()=>{
+    alert("go to learn python")
+    // document.write("this site is not working yet!!!!")
+}
+ 
